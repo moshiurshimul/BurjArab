b2fec19ec1e62c1f4e56f058f273e453177fd11a
moshiurshimul <hello@moshiurshimul.com>
2020-10-02 03:41:53 +0600

    route added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,19 +5,35 @@ import Button from '@material-ui/core/Button';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 
 
 const Login = () => {
+    // Use Context API
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    // User redirect after sign in
+    const history = useHistory();
+    const location = useLocation();
+
+    const { from } = location.state || { from: { pathname: "/" } };
 
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    if(firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    };
 
     const handleGoogleSignIn = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
             var token = result.credential.accessToken;
-            var user = result.user;
-            console.log(user)
+            const {displayName, email} = result.user;
+            const signInUser = {name: displayName, email: email};
+            console.log(signInUser)
+            setLoggedInUser(signInUser);
+            history.replace(from);
+            
           }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
